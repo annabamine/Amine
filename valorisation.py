6b25ad0d67560a6f89289cb1c6cd338587cd5acc
annabamine <annabamine1997@gmail.com>
2026-06-17 07:48:43 +0200

    Update valorisation.py

--- valorisation.py
+++ valorisation.py
@@ -130,7 +130,7 @@ def fetch_search_results(query):
     except:
         return []
         
-search_query = st.text_input("🔍 Rechercher une entreprise (nom ou ticker)", "Nvidia", key="main_search_query")
+search_query = st.text_input("🔍 Rechercher une entreprise (nom ou ticker)", "Nvda", key="main_search_query")
 
 if search_query:
     try:
